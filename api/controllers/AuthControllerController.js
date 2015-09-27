b7fa8c7a70a92cc105a909e56b307ffa731523c0
Sam <sam@concept96.co.uk>
2015-09-27 21:36:26 +0100

    time for some auth

---
+++ api/controllers/AuthControllerController.js
@@ -0,0 +1,31 @@
+/**
+ * AuthControllerController
+ *
+ * @description :: Server-side logic for managing Authcontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `AuthControllerController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `AuthControllerController.logout()`
+   */
+  logout: function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  }
+};
+
