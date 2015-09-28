d82189fb8f9365134edf4abbd64bea54104f484b
Sam <sam@concept96.co.uk>
2015-09-28 14:49:48 +0100

    clean up (goodbye waterlock)

---
+++ api/models/Employee.js
@@ -0,0 +1,24 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	migrate: "drop",
+	scheme: true,
+	connection: "mongodb",
+	autoPK: true,
+	autoCreatedAt: false,
+	autoUpdatedAt: false,
+	attributes: {
+		name: {
+			type: "string",
+			string: true,
+			required: true,
+			maxLength: 128,
+			empty: false
+		}
+	}
+};
